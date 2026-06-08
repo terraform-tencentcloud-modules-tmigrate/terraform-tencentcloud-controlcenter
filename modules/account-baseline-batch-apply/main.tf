8248830fa5843d2f8613a60b4329ca79d0eafdf8
MaDongdong <donaldma@tencent.com>
2026-06-08 19:59:32 +0800

    refactor: for_each over member_uin_list, one resource per uin in account-baseline-batch-apply

--- modules/account-baseline-batch-apply/main.tf
+++ modules/account-baseline-batch-apply/main.tf
@@ -1,16 +1,17 @@
 locals {
-  baseline_config_items_map = {
-    for item in var.baseline_config_items : item.identifier => item
-  }
+  member_uin_map = { for uin in var.member_uin_list : tostring(uin) => uin }
 }
 
 resource "tencentcloud_batch_apply_account_baselines" "baselines" {
-  for_each = local.baseline_config_items_map
+  for_each = local.member_uin_map
 
-  member_uin_list = var.member_uin_list
+  member_uin_list = [each.value]
 
-  baseline_config_items {
-    identifier    = each.value.identifier
-    configuration = each.value.configuration
+  dynamic "baseline_config_items" {
+    for_each = var.baseline_config_items
+    content {
+      identifier    = baseline_config_items.value.identifier
+      configuration = baseline_config_items.value.configuration
+    }
   }
 }
